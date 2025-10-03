51d1ee3d9dfc1f12303428689493d31c481315dc
Christian <stipp2@gmail.com>
2025-10-03 15:43:54 -0300

    projeto salario e beneficios

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Desafio
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digitar o valor do salario: ");
+        float wage = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digitar o valor do beneficio: ");
+        float valore = float.Parse(Console.ReadLine());
+
+        float impost = 0;
+        if (wage >= 0 && wage <= 1100)
+        {
+            impost = 0.05F * wage;
+            Console.WriteLine("Valor do salário é: " + wage);
+        } else if (wage >= 1100 && wage <= 2500)
+        {
+            impost = 0.10F * wage;
+            Console.WriteLine("Valor do salário é: " + wage);
+        } else
+        {
+            impost = 0.15F * wage;
+            Console.WriteLine("Valor do salário é: " + wage);
+        }
+
+        float exit = wage - impost + valore;
+        Console.WriteLine(exit.ToString("0.00"));
+    }
+}
